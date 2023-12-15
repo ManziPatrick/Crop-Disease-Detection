cb7c4065689543111d4ae738df0c0add3f8c863f
ManziPatrick <144239912+ManziPatrick@users.noreply.github.com>
2023-12-15 11:58:42 +0200

    Update app.py
    crop

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-
 import streamlit as st
+from tensorflow import keras
 import cv2
 import numpy as np
-from tensorflow import keras
 
-def load_model(file_path):
-    
-    model = keras.models.load_model(file_path)
-    return model
+# Load the Keras model
+model = model = keras.models.load_model('model.h5')
 
 def preprocess_image(image):
     image = cv2.resize(image, (128, 128))
-    image = image / 255.0
+    image = image / 255.0 
     return image
 
-def predict(image, model):
+def predict(image):
     try:
-        # Placeholder function for the prediction logic (without TensorFlow)
-        # You can replace this with any logic based on your requirements
-        # For simplicity, let's say it always predicts 'Not Affected'
-        return 'Not Affected'
+        # Preprocess the image
+        preprocessed_image = preprocess_image(image)
+
+        # Make prediction
+        input_image_reshaped = np.reshape(preprocessed_image, [1, 128, 128, 3])
+        prediction = model.predict(input_image_reshaped)
+        result = np.argmax(prediction)
+
+        if result == 1:
+            prediction_label = 'Affected'
+        else:
+            prediction_label = 'Not Affected'
+
+        return prediction_label
 
     except Exception as e:
         return str(e)
@@ -27,33 +34,22 @@ def predict(image, model):
 def main():
     st.title('Crop Disease Detection')
 
-    # File uploader for the model
-    model_file = st.file_uploader("Upload a model file (.h5)", type=["h5"])
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-    if model_file is not None:
-        # Load the model
-        model = load_model(model_file)
+    if uploaded_file is not None:
+        try:
+            # Read the image
+            image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
 
-        # File uploader for the image
-        uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+            # Display the uploaded image
+            st.image(image, caption='Uploaded Image', use_column_width=True)
 
-        if uploaded_file is not None:
-            try:
-                # Read the image
-                image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+            # Make prediction
+            prediction = predict(image)
 
-                # Display the uploaded image
-                st.image(image, caption='Uploaded Image', use_column_width=True)
+            # Display the prediction result
+            st.write(f'Prediction: {prediction}')
 
-                # Make prediction (placeholder)
-                prediction = predict(image, model)
-
-                # Display the prediction result
-                st.write(f'Prediction: {prediction}')
-
-            except Exception as e:
-                # Display any exception that occurs during image processing
-                st.error(f"Error: {str(e)}")
-
-if __name__ == '__main__':
-    main()
+        except Exception as e:
+            # Display any exception that occurs during image processing
+            st.error(f"Error: {str(e)}")
