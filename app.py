182c82b8c08dfadb885baf5707df385f083b9fa1
ManziPatrick <144239912+ManziPatrick@users.noreply.github.com>
2024-07-23 17:00:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,17 @@
-
-import streamlit as st 
-import pandas as pd
 import streamlit as st
-import time
 import numpy as np
 import pandas as pd
-from PIL import Image 
-
-st.title('Crop Image Detection Demo :blue[cassava] :sunglasses:')
-
-    
-import streamlit as st
-
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-
-
-
-
-
-import streamlit as st
-
-# Function to display content for Link 1
-def display_link1_content():
-    st.write("hello.")
-
-# Function to display content for Link 2
-def display_link2_content():
-    st.write("world.")
-
-# Main function to create the Streamlit app
-def main():
-    # Add clickable links to the sidebar
-    st.sidebar.write('### Links')
-    link1_clicked = st.sidebar.button('home')
-    link2_clicked = st.sidebar.button('contact')
-
-    # Display content based on which link is clicked
-    if link1_clicked:
-        display_link1_content()
-    elif link2_clicked:
-        display_link2_content()
-
-# Call the main function to run the Streamlit app
-if __name__ == '__main__':
-    main()
-
-      
-
-
-import streamlit as st
+from PIL import Image
 import cv2
 import tensorflow as tf
-import numpy as np
 
-# Define function to preprocess image
+# Function to preprocess image
 def preprocess_image(image):
     image = cv2.resize(image, (128, 128))
-    image = image / 255.0 
+    image = image / 255.0
     return image
 
-# Define function to load and make predictions with the model
+# Function to load and make predictions with the model
 def predict(image):
     try:
         # Load the Keras model
@@ -88,10 +35,36 @@ def predict(image):
     except Exception as e:
         return str(e)
 
-# Define main function to create Streamlit app
-def main():
-    st.title('Crop Disease Detection')
+# Function to display content for Link 1
+def display_link1_content():
+    st.write("hello.")
 
+# Function to display content for Link 2
+def display_link2_content():
+    st.write("world.")
+
+# Main function to create Streamlit app
+def main():
+    st.title('Crop Disease Detection Demo :blue[cassava] :sunglasses:')
+
+    # Sidebar selectbox
+    add_selectbox = st.sidebar.selectbox(
+        "How would you like to be contacted?",
+        ("Email", "Home phone", "Mobile phone")
+    )
+
+    # Add clickable links to the sidebar
+    st.sidebar.write('### Links')
+    link1_clicked = st.sidebar.button('home')
+    link2_clicked = st.sidebar.button('contact')
+
+    # Display content based on which link is clicked
+    if link1_clicked:
+        display_link1_content()
+    elif link2_clicked:
+        display_link2_content()
+
+    # File uploader for image
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
